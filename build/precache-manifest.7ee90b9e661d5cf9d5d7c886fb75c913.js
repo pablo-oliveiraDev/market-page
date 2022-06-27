79a9bbef59adcf3b70fbf4ee53ef288c8c17f962
Pablo de Oliveira Ferreira <67244309+pabloliverfe-stack@users.noreply.github.com>
2022-06-27 13:55:44 -0300

    Add files via upload
    add pelo upload files

---
+++ build/precache-manifest.7ee90b9e661d5cf9d5d7c886fb75c913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c871650366fa291c346",
+    "url": "/static/css/main.810fdaa5.chunk.css"
+  },
+  {
+    "revision": "2c871650366fa291c346",
+    "url": "/static/js/main.6c3f3677.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e373b307217449a4e58e",
+    "url": "/static/css/2.e42112bd.chunk.css"
+  },
+  {
+    "revision": "e373b307217449a4e58e",
+    "url": "/static/js/2.f1e26153.chunk.js"
+  },
+  {
+    "revision": "5ccb298d7752a2cd8e44076122890516",
+    "url": "/index.html"
+  }
+];
